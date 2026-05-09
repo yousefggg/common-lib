53348c99f4aadf9a0fe7f8f2f0a4631ddb75135d
Yusef <mulaev2006@gmail.com>
2026-05-09 10:56:44 +0400

    Update file jwt

--- pkg/jwt/jwt.go
+++ pkg/jwt/jwt.go
@@ -33,3 +33,40 @@ func (m *TokenManager) GenerateToken(userID uuid.UUID, role string) (string, err
 
 	return token.SignedString([]byte(m.secretKey))
 }
+
+func (m *TokenManager) ValidateToken(tokenString string) (uuid.UUID, string, error) {
+
+    token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+
+        if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+            return nil, errors.New("unexpected signing method")
+        }
+        return []byte(m.secretKey), nil
+    })
+
+    if err != nil {
+        return uuid.Nil, "", err
+    }
+
+    if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+    
+        sub, ok := claims["sub"].(string)
+        if !ok {
+            return uuid.Nil, "", errors.New("invalid subject in token")
+        }
+
+        userID, err := uuid.Parse(sub)
+        if err != nil {
+            return uuid.Nil, "", errors.New("invalid user id format")
+        }
+
+        role, ok := claims["role"].(string)
+        if !ok {
+            return uuid.Nil, "", errors.New("role not found in token")
+        }
+
+        return userID, role, nil
+    }
+
+    return uuid.Nil, "", errors.New("invalid token claims")
+}
